d26072fcf516a382ed84cd1c77c4c5b7d11d2504
Jordan Lapid <jordanlapid@gmail.com>
2017-08-18 17:13:03 -0700

    hangman updates

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,5 @@
-//Create array to store all possible words
-//Create variable to track wins
-//Create variable to track number of guesses available
-//Create array to store letters that were already guessed
-var wordList = ["khaleesi","daenarys","jonsnow","targaryen","stark","lannister","greyjoy","dorne","nightking","wight","tyrion","thehound","themountain","cersei","jamie","tyrell","whitewalker"];
+
+var wordList = ["khaleesi","daenarys","jonsnow","targaryen","stark","lannister","greyjoy","dorne","nightking","wight","tyrion","thehound","themountain","cersei","jamie","tyrell","whitewalker","dothraki","khalasar","valyrian","winterfell","highgarden","drownedgod","valarmorgulis","ironthrone","wildling","mancerayder","azorahai","rhaegar","khaldrogo","martell","sunspear","ghost","nymeria","shaggydog","longclaw","lady","baratheon"];
 var totalWins = 0;
 var totalLosses = 0;
 var guessesLeft = 9;
@@ -10,10 +7,72 @@ var guessedLetters =[];
 var word = wordList[Math.floor(Math.random()*wordList.length)];
 var currentWord = word.split("");
 var underscore = Array(currentWord.length+1).join("-");
+var gameThemeSong = document.getElementById("gameTheme");
+var youLose = document.getElementById("youLose");
+var gangnam = document.getElementById("gangnamStyle");
+
+hidePsy();
+document.getElementById("gLeft").innerHTML = guessesLeft;
+document.getElementById("cWord").innerHTML = underscore;
+document.getElementById("gLetters").innerHTML = guessedLetters;
+document.getElementById("tWins").innerHTML = totalWins;
+document.getElementById("tLosses").innerHTML = totalLosses;
+
+
+function hidePsy(){
+	document.getElementById("psy").style.visibility = "hidden"
+}
+
+function showPsy (){
+	setTimeout(function(){
+	    document.getElementById("psy").style.visibility = "visible";
+	}, 3200);
+	
+}
+
+function changePicture (){
+	setTimeout(function(){
+	document.getElementById("secondPic").src ="https://fontmeme.com/permalink/170818/4d9cf1fe92fe1ab0adeeacf99525f5f4.png";
+	}, 3200);
+}
+
+function changePictureBack(){
+	document.getElementById("secondPic").src ="https://fontmeme.com/permalink/170816/e3fe4dfce4722d2b2fba0380788b6b10.png";
+
+}
+
+function pauseGangnam (){
+	gangnam.currentTime = 0;
+	gangnam.pause();
+}
+
+
+function playGangnam (){
+	gangnam.currentTime = 150.4;
+	gangnam.play();
+}
+
+function playYouLose(){
+	youLose.play();
+	setTimeout(function(){
+	    youLose.pause();
+	    youLose.currentTime = 0;
+	}, 1500);
+}
+
+function playGameTheme(){
+	gameThemeSong.play();
+}
+
+function pauseGameTheme(){
+	gameThemeSong.currentTime = 0;
+	gameThemeSong.pause();
+}
 
 document.onkeyup = function (event) {
 	var keyHit = event.key;
 	playGame(keyHit);
+	console.log(keyHit);
 }
 
 
@@ -32,49 +91,63 @@ function resetGame (){
 }
 
 function playGame (keyHit){
-	document.getElementById("pAgain").innerHTML = "Good Luck!";
-	document.getElementById("cWord").innerHTML = underscore;
+
 	console.log(currentWord);
 
-	for(var i = 0; i < currentWord.length; i++){
-		if(keyHit === currentWord[i]){
-			underscore = setCharAt(underscore,i,currentWord[i]);
-			document.getElementById("cWord").innerHTML = underscore;
+	if(guessesLeft > 0 && underscore != word){
 
+		playGameTheme();
+
+		for(var i = 0; i < currentWord.length; i++){
+			if(keyHit === currentWord[i]){
+				underscore = setCharAt(underscore,i,currentWord[i]);
+				document.getElementById("cWord").innerHTML = underscore;
+
+			}
+		}
+			if(!currentWord.includes(keyHit) && !guessedLetters.includes(keyHit) && keyHit != "Enter"){
+				guessesLeft = guessesLeft -1;
+				document.getElementById("gLeft").innerHTML = guessesLeft;
+			}
+
+
+			if(keyHit && keyHit != "Enter") {
+				if(!guessedLetters.includes(keyHit) ){
+				guessedLetters.push(keyHit); 
+			}
+				document.getElementById("gLetters").innerHTML = guessedLetters;
+			}
+
+			if(guessesLeft === 0){
+				totalLosses = totalLosses + 1;
+				document.getElementById("tLosses").innerHTML = totalLosses;
+				pauseGameTheme();
+				playYouLose();
+				document.getElementById("pAgain").innerHTML = "Press Enter to Play Again!";
+			}
+
+			if(underscore === word){
+				totalWins = totalWins + 1;
+				document.getElementById("tWins").innerHTML = totalWins;
+				pauseGameTheme();
+				playGangnam();
+				changePicture();
+				showPsy();
+				setTimeout(function(){ document.getElementById("pAgain").innerHTML = "Press Enter to Play Again!"; }, 3200);
+				
+			}
+
+	} else {
+			if(keyHit === "Enter"){
+				pauseGangnam();
+				changePictureBack();
+				hidePsy();
+				resetGame();
+				document.getElementById("pAgain").innerHTML = "Good Luck!";
+				document.getElementById("cWord").innerHTML = underscore;
+				document.getElementById("gLetters").innerHTML = guessedLetters;
+				document.getElementById("gLeft").innerHTML = guessesLeft;
 		}
 	}
-		if(!currentWord.includes(keyHit) && !guessedLetters.includes(keyHit)){
-			guessesLeft = guessesLeft -1;
-			document.getElementById("gLeft").innerHTML = guessesLeft;
-		}
-
-
-		if(keyHit) {
-			if(!guessedLetters.includes(keyHit) ){
-			guessedLetters.push(keyHit); 
-		}
-			document.getElementById("gLetters").innerHTML = guessedLetters;
-		}
-
-		if(guessesLeft === 0){
-			totalLosses = totalLosses + 1;
-			document.getElementById("tLosses").innerHTML = totalLosses;
-			resetGame();
-			document.getElementById("cWord").innerHTML = underscore;
-			document.getElementById("gLetters").innerHTML = guessedLetters;
-			document.getElementById("gLeft").innerHTML = guessesLeft;
-			document.getElementById("pAgain").innerHTML = "Press any Key to Play Again!";
-		}
-
-		if(underscore === word){
-			totalWins = totalWins + 1;
-			document.getElementById("tWins").innerHTML = totalWins;
-			resetGame();
-			document.getElementById("tWins").innerHTML = totalWins;
-			document.getElementById("gLetters").innerHTML = guessedLetters;
-			document.getElementById("gLeft").innerHTML = guessesLeft;
-			document.getElementById("pAgain").innerHTML = "Press any Key to Play Again!";
-		}
-
 }
 
